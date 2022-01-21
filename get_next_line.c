a95645835a494c5aea40c10374dbb7ebfd7f7eea
ikarjala <ikarjala@student.hive.fi>
2022-01-22 00:16:30 +0200

    init commit: add author, basic src files and eval_test conveniences

---
+++ get_next_line.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikarjala <ikarjala@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 17:45:37 by ikarjala          #+#    #+#             */
+/*   Updated: 2022/01/21 17:48:00 by ikarjala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	get_next_line(const int fd, char **line)
+{
+    read(fd, *line, BUFF_SIZE);
+    return (RET_EOF);
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikarjala <ikarjala@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 17:46:50 by ikarjala          #+#    #+#             */
+/*   Updated: 2022/01/21 17:47:59 by ikarjala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <libft.h>
+
+#define RET_READL	1
+#define RET_EOF	0
+#define RET_ERROR	-1
+
+#define BUFF_SIZE	32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
