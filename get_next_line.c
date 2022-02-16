ee2b485e0aea10a05800597a219b6df46286f90c
ikarjala <ikarjala@c1r1p2.hive.fi>
2022-02-16 18:43:46 +0200

    regret, rethink, yeet

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 17:45:37 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/02/11 18:05:00 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/02/16 18:41:12 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static size_t	ft_strclen(const char* str, int c)
 	size_t	len;
 
 	len = 0;
-	while (*str != '\0')
+	while (*str)
 	{
 		if (*str++ == c)
 			break ;
@@ -26,39 +26,22 @@ static size_t	ft_strclen(const char* str, int c)
 	return (len);
 }
 
-static char	*ft_enumerate(char *s, int (*fn)(int), size_t len)
-{
-	while (len-- > 0)
-	{
-		if (fn(*s))
-			return (s);
-	}
-	return (NULL);
-}
-
 int	get_next_line(const int fd, char **line)
 {
 	static char	buf[BUFF_SIZE + 1];
 	size_t		i_nl;
 	ssize_t		bytes;
-	t_uint		bookmrk;
 
 	if (!fd || !line)
 		return (RET_ERROR);
-	bytes = 0;
-	if (!buf[BUFF_SIZE - 1])
-		bytes = read(fd, buf, BUFF_SIZE);
-	else
-		bookmrk = ft_enumerate(buf, &ft_isnonzero, BUFF_SIZE);
-	if (bytes == 0)
-		return (RET_EOF);
+	buf[BUFF_SIZE] = 0;
 	i_nl = ft_strclen(buf, '\n');
-	if (*line)
-		free(*line);
-	*line = ft_strsub(buf, 0, i_nl);
+	while (i_nl == BUFF_SIZE)
+	{
+		bytes = read(fd, buf, BUFF_SIZE);
+		if (bytes == 0)
+			return (RET_EOF);
+		*line = ft_strsub(buf, 0, i_nl);
+	}
 	return (RET_READL);
 }
-
-// TODO: need a way to find where we left off if buffer is not empty
-// gist: change each nl to null as we go
-// enumerate and bzero until we find null char
